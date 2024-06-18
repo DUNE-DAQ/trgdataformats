cad9769070ddd1ff58dde5a0c2fb532a7749e59b
Kurt Biery <biery@daq.fnal.gov>
2024-06-18 06:52:14 -0500

    another attempt at naming the ICEBERG-related TC types

--- include/trgdataformats/TriggerCandidateData.hpp
+++ include/trgdataformats/TriggerCandidateData.hpp
@@ -57,8 +57,8 @@ struct TriggerCandidateData
     kCTBCustomB = 33,
     kCTBCustomC = 34,
     kCTBCustomPulseTrain = 35,
-    kTLUFakeTrigger = 36,
-    kICEBERGCosmic = 37,
+    kDTSPulser = 36,
+    kDTSCosmic = 37,
   };
 
   enum class Algorithm
@@ -136,8 +136,8 @@ get_trigger_candidate_type_names()
     { TriggerCandidateData::Type::kCTBCustomB, "kCTBCustomB" },
     { TriggerCandidateData::Type::kCTBCustomC, "kCTBCustomC" },
     { TriggerCandidateData::Type::kCTBCustomPulseTrain, "kCTBCustomPulseTrain" },
-    { TriggerCandidateData::Type::kTLUFakeTrigger, "kTLUFakeTrigger" },
-    { TriggerCandidateData::Type::kICEBERGCosmic, "kICEBERGCosmic" },
+    { TriggerCandidateData::Type::kDTSPulser, "kDTSPulser" },
+    { TriggerCandidateData::Type::kDTSCosmic, "kDTSCosmic" },
   };
 }
 
