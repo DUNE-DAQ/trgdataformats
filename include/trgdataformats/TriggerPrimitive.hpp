a8aac59394a5ea24764d9a638b00b5e86ec44e8d
Alejandro Oranday <alejandro@oran.day>
2025-03-05 10:43:05 +0100

    Set TP Flag Default.

--- include/trgdataformats/TriggerPrimitive.hpp
+++ include/trgdataformats/TriggerPrimitive.hpp
@@ -25,7 +25,7 @@ namespace dunedaq::trgdataformats {
 struct TriggerPrimitive
 {
   static constexpr uint8_t s_trigger_primitive_version = 2;
-  uint64_t version : 8 = s_trigger_primitive_version, flag : 8, detid : 8; // 24 out of 64 bits.
+  uint64_t version : 8 = s_trigger_primitive_version, flag : 8 = 0, detid : 8; // 24 out of 64 bits.
 
   uint64_t channel : 24, time_peak : 16;  // Locations. Fills the remaining bits.
   uint64_t time_start : 64;
