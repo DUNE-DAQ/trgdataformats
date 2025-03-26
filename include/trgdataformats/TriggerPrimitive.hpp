3ad29748890804a44fe69e4d9a434d5b837d41a6
Wesley Robert Ketchum <wketchum@fnal.gov>
2025-03-26 17:41:01 +0100

    remove bit initialization and add default constructor so it works with C++17

--- include/trgdataformats/TriggerPrimitive.hpp
+++ include/trgdataformats/TriggerPrimitive.hpp
@@ -28,19 +28,31 @@ struct TriggerPrimitive
   static constexpr uint8_t s_trigger_primitive_version = 2;
 
   // Metadata.
-  uint64_t version : 8 = s_trigger_primitive_version;
-  uint64_t flag : 8 = 0;
-  uint64_t detid : 8 = INVALID_DETID;
+  uint64_t version : 8;
+  uint64_t flag : 8;
+  uint64_t detid : 8;
 
   // Physics data.
-  uint64_t channel : 24 = INVALID_TP_CHANNEL;
+  uint64_t channel : 24;
 
-  uint64_t samples_over_threshold : 16 = INVALID_SAMPLES_OVER_THRESHOLD;
-  uint64_t time_start : 64 = INVALID_TIMESTAMP;
-  uint64_t samples_to_peak : 16 = INVALID_SAMPLES_TO_PEAK;
+  uint64_t samples_over_threshold : 16;
+  uint64_t time_start : 64;
+  uint64_t samples_to_peak : 16;
 
-  uint64_t adc_integral : 32 = 0;
-  uint64_t adc_peak : 16 = 0;
+  uint64_t adc_integral : 32;
+  uint64_t adc_peak : 16;
+
+  TriggerPrimitive()
+    : version(s_trigger_primitive_version)
+    , flag(0)
+    , detid(INVALID_DETID)
+    , channel(INVALID_TP_CHANNEL)
+    , samples_over_threshold(INVALID_SAMPLES_OVER_THRESHOLD)
+    , time_start(INVALID_TIMESTAMP)
+    , samples_to_peak(INVALID_SAMPLES_TO_PEAK)
+    , adc_integral(0)
+    , adc_peak(0)
+  {}
 };
 
 } // namespace dunedaq::trgdataformats
